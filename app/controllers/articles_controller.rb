49b0d6c338da71c962e926d4544d409216d0755a
shou.yamamoto <ec2-user@ip-172-31-89-148.ec2.internal>
2020-03-30 08:56:38 +0000

    .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,8 +29,11 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(article_params)
-    redirect_to @article
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   def destroy
