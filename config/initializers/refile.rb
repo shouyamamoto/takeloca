a37c9c7ecdf931380c92000b2c9b7c02c2c42969
shou.yamamoto <ec2-user@ip-172-31-89-148.ec2.internal>
2020-04-02 05:16:44 +0000

    .

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,9 @@
 require "refile/s3"
-
 aws = {
-  access_key_id: ENV["ACCESS_KEY_ID"],
-  secret_access_key: ENV["SECRET_ACCESS_KEY"],
+  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
   region: "s3-ap-northeast-1",
-  bucket: "takeloca2"
+  bucket: "takeloca2",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
