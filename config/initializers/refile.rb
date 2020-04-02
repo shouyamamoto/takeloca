8bfdf8fe066a392a6b6ad12748ba1c6786b81c4c
shou.yamamoto <ec2-user@ip-172-31-89-148.ec2.internal>
2020-04-02 05:43:46 +0000

    .

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,4 +1,4 @@
-equire "refile/s3"
+require "refile/s3"
 aws = {
   access_key_id: Rails.application.credentials.aws[:access_key_id],
   secret_access_key: Rails.application.credentials.aws[:secret_access_key],
