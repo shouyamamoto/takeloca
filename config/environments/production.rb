9f7512209d0da6f9b690fb25d28acb248a90e670
shou.yamamoto <ec2-user@ip-172-31-89-148.ec2.internal>
2020-04-02 07:54:50 +0000

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.force_ssl = true
+  
+  config.assets.initialize_on_precompile = false
 end
--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,7 +3,7 @@ require "refile/s3"
 aws = {
   access_key_id: Rails.application.credentials.aws[:access_key_id],
   secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-  region: "s3-ap-northeast-1",
+  region: "ap-northeast-1",
   bucket: "takeloca2",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
